ef4038f7dff13fb9ca150af97e3da9c5d9cfe27f
guilhermerodrigues680 <guilhermerodrigues680@gmail.com>
2022-08-01 06:56:23 -0300

    deploy gh-pages

---
+++ deploy-gh-pages.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env sh
+
+# GitHub Pages - Deploying a Static Site | Vite
+# https://vitejs.dev/guide/static-deploy.html#github-pages
+
+# abort on errors
+set -e
+
+# build
+npm run build
+
+# navigate into the build output directory
+cd dist
+
+# if you are deploying to a custom domain
+# echo 'www.example.com' > CNAME
+
+# bypass Jekyll processing on GitHub Pages
+echo '' > .nojekyll
+
+git init
+git checkout -b main
+git add -A
+git commit -m 'deploy'
+
+# if you are deploying to https://<USERNAME>.github.io
+# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
+
+# if you are deploying to https://<USERNAME>.github.io/<REPO>
+git push -f git@github.com:guilhermerodrigues680/vuejs2-nicole-demonstracao.git main:gh-pages
+
+# OBS: Lembrar de verificar `publicPath` no `vue.config.js`
+
+cd -
--- src/main.js
+++ src/main.js
@@ -4,6 +4,8 @@ import router from "./router";
 
 Vue.config.productionTip = false;
 
+console.debug("publicPath (base):", process.env.BASE_URL);
+
 new Vue({
   router,
   render: (h) => h(App),
